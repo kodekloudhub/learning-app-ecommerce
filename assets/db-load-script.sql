06b2618e63bfe105baa4774719017ff4ee4a18b3
Mumshad Mannambeth <mmumshad@gmail.com>
2019-10-11 19:01:32 +0800

    Create db-load-script.sql

---
+++ assets/db-load-script.sql
@@ -0,0 +1,5 @@
+USE ecomdb;
+CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
+
+INSERT INTO products (Name,Price,ImageUrl) VALUES ("Laptop","100","c-1.png"),("Drone","200","c-2.png"),("VR","300","c-3.png"),("Tablet","50","c-5.png"),("Watch","90","c-6.png"),("Phone Covers","20","c-7.png"),("Phone","80","c-8.png"),("Laptop","150","c-4.png");
+
