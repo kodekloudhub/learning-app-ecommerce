2f25ad1a78378ff6c69c3504bc76d5746677025d
mourad lakhtibi <spacetheplace@hotmail.com>
2022-09-25 17:39:46 +0200

    setting server to lacal

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
             <div class="row it_works">
               <?php
 
-                        $link = mysqli_connect('172.20.1.101', 'ecomuser', 'ecompassword', 'ecomdb');
+                        $link = mysqli_connect('localhost', 'ecomuser', 'ecompassword', 'ecomdb');
 
                         if ($link) {
                         $res = mysqli_query($link, "select * from products;");
