c3cb90648563febf0f05f4753a78803a2708edb3
mourad lakhtibi <60454104+mouradlakhtibi@users.noreply.github.com>
2023-08-14 18:06:37 +0200

    Update index.php
    testing change on index file

--- files/index.php
+++ files/index.php
@@ -89,7 +89,7 @@
         <section class="slider_area row m0">
             <div class="slider_inner">
                 <div class="camera_caption">
-                    <h2 class="wow fadeInUp animated">Make Your Shopping Easy</h2>
+                    <h2 class="wow fadeInUp animated">throw your app Easy</h2>
                     <h5 class="wow fadeIn animated" data-wow-delay="0.3s">Find everything accordingly</h5>
                     <a class="learn_mor wow fadeInU" data-wow-delay="0.6s" href="#product-list">Show Now!</a>
                 </div>
